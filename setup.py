1f04dc6e3e1d73d62ef8aaf42a4436140775ad44
Andreas Motl <andreas.motl@elmyra.de>
2016-07-10 01:31:33 +0200

    Bump version: 0.5.0 → 0.6.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='beradio',
-    version='0.5.0',
+    version='0.6.0',
     description='BERadio',
     long_description='BERadio spec and reference implementation',
     classifiers=[
