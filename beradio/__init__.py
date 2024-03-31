1e70455a6671e2091e919b212b7de5d3b53897d7
Andreas Motl <andreas@hiveeyes.org>
2024-03-31 18:59:13 +0200

    Bump version: 0.13.1 → 0.14.0

--- beradio/__init__.py
+++ beradio/__init__.py
@@ -3,7 +3,7 @@
 # (c) 2015 Andreas Motl <andreas@hiveeyes.org>
 """BERadio spec and reference implementation"""
 __appname__ = "beradio"
-__version__ = "0.13.1"
+__version__ = "0.14.0"
 
 
 def program_name(with_version=False):
